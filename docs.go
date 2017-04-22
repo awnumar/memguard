a447a41103d728a39a34d93d91e5ea3543eee0df
Awn <libeclipse@gmail.com>
2017-04-22 09:47:35 +0100

    Update documentation

--- docs.go
+++ docs.go
@@ -1,15 +1,13 @@
 /*
 Package memguard is designed to allow you to easily handle sensitive values in memory. The main functionality is to lock and watch portions of memory and wipe them on exit, but there are some supplementary function too.
 
-Multiple operating systems are supported, using `mlock` on Unix and `VirtualLock` on Windows.
-
     // Declare a protected slice and copy into it.
     encryptionKey := memguard.MakeProtected(32)
     encryptionKey = _generateRandomBytes(32)
 
-It is important to never use append() with sensitive values. Assign directly or call copy() instead.
+Please note that it is important to never use append() with sensitive values. Assign directly or call copy() instead.
 
-Although this is less recommended, sometimes it is necessary when the length of the slice is not known in advance. For example, when accepting user input.
+When you do not know the length of the data in advance, you may have to allocate first and then protect, even though this is not recommended. An example is accepting user input.
 
     password := input() // Some arbitrary input function.
     memguard.Protect(password)
@@ -23,7 +21,7 @@ When you're about to exit, call cleanup first. This will wipe and then unlock al
 
     memguard.Cleanup()
 
-// It's useful to capture interrupts and signals and cleanup in that case too.
+It's useful to capture interrupts and signals and cleanup in that case too.
 
     c := make(chan os.Signal, 2)
     signal.Notify(c, os.Interrupt, syscall.SIGTERM)
