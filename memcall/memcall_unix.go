db2ebcf68044f31fee7f4c4a0fc5b546ac20ef48
Awn <libeclipse@gmail.com>
2017-04-24 18:09:27 +0100

    Implemented all of the required syscalls on both Unix and Windows

--- memcall/memcall_unix.go
+++ memcall/memcall_unix.go
@@ -12,20 +12,59 @@ import (
 func Init() {
 	// Disable core dumps.
 	if err := unix.Setrlimit(unix.RLIMIT_CORE, &unix.Rlimit{Cur: 0, Max: 0}); err != nil {
-		panic(fmt.Sprintf("memguard.memprot.init(): could not set rlimit [Err: %s]", err))
+		panic(fmt.Sprintf("memguard.memprot.Init(): could not set rlimit [Err: %s]", err))
 	}
 }
 
 // Lock is a wrapper for unix.Mlock(), with extra precautions.
 func Lock(b []byte) {
 	if err := unix.Mlock(b); err != nil {
-		panic(fmt.Sprintf("memguard.memcall.Lock(): could not aquire lock on %p", &b[0]))
+		panic(fmt.Sprintf("memguard.memcall.Lock(): could not aquire lock on %p [Err: %s]", &b[0], err))
 	}
 }
 
 // Unlock is a wrapper for unix.Unlock.
 func Unlock(b []byte) {
 	if err := unix.Munlock(b); err != nil {
-		panic(fmt.Sprintf("memguard.memcall.Unlock(): could not free lock on %p", &b[0]))
+		panic(fmt.Sprintf("memguard.memcall.Unlock(): could not free lock on %p [Err: %s]", &b[0], err))
+	}
+}
+
+// Alloc allocates a byte slice of length n and returns it.
+func Alloc(n int) []byte {
+	// Allocate the memory.
+	b, err := unix.Mmap(-1, 0, n, unix.PROT_NONE, unix.MAP_PRIVATE|unix.MAP_ANONYMOUS)
+	if err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Alloc(): could not allocate [Err: %s]", err))
+	}
+
+	// Advise the kernel not to dump. Ignore failure.
+	unix.Madvise(b, 0x10)
+
+	// Return the allocated memory.
+	return b
+}
+
+// Free wipes and unallocates the byte slice specified.
+func Free(b []byte) {
+	// Wipe it first.
+	for i := 0; i < len(b); i++ {
+		b[i] = byte(0)
+	}
+
+	// Unallocate it.
+	if err := unix.Munmap(b); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Free(): could not unallocate %p [Err: %s]", &b[0], err))
+	}
+
+	// Set it to nil.
+	b = nil
+	_ = b
+}
+
+// Protect modifies the PROT_ flags for a specified byte slice.
+func Protect(b []byte, prot int) {
+	if err := unix.Mprotect(b, prot); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Protect(): could not set %d on %p [Err: %s]", prot, &b[0], err))
 	}
 }
--- memcall/memcall_windows.go
+++ memcall/memcall_windows.go
@@ -7,33 +7,84 @@ import (
 	"unsafe"
 
 	"golang.org/x/sys/windows"
+
+	"github.com/alexbrainman/winapi"
 )
 
-// Single-word zero for use when we need a valid pointer to 0 bytes.
-var _zero uintptr
+// Placeholder variable for when we need a valid pointer to zero bytes.
+var _zero uintpt
+
+// Init is included for compatibility between Unix and Windows. It is a No-Op function.
+func Init() {}
 
 // Lock is a wrapper for windows.VirtualLock()
 func Lock(b []byte) {
-	var _p0 unsafe.Pointer
-	if len(b) > 0 {
-		_p0 = unsafe.Pointer(&b[0])
-	} else {
-		_p0 = unsafe.Pointer(&_zero)
-	}
-	if err := windows.VirtualLock(uintptr(_p0), uintptr(len(b))); err != nil {
-		panic(fmt.Sprintf("memguard.memcall.Lock(): could not aquire lock on %p", &b[0]))
+	if err := windows.VirtualLock(_getPtr(b), uintptr(len(b))); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Lock(): could not aquire lock on %p [Err: %s]", &b[0], err))
 	}
 }
 
 // Unlock is a wrapper for windows.VirtualUnlock()
 func Unlock(b []byte) {
+	if err := windows.VirtualUnlock(_getPtr(b), uintptr(len(b))); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Unlock(): could not free lock on %p [Err: %s]", &b[0], err))
+	}
+}
+
+// Alloc allocates a byte slice of length n and returns it.
+func Alloc(n int) []byte {
+	// Allocate the memory.
+	ptr, err := winapi.VirtualAlloc(nil, n, 0x00001000, 0x01)
+	if err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Alloc(): could not allocate [Err: %s]", err))
+	}
+
+	// Return the allocated memory.
+	return _getBytes(ptr, n, n)
+}
+
+// Free wipes and unallocates the byte slice specified.
+func Free(b []byte) {
+	// Wipe it first.
+	for i := 0; i < len(b); i++ {
+		b[i] = byte(0)
+	}
+
+	// Unallocate it.
+	if err != winapi.VirtualFree(_getPtr(b), uintptr(len(b)), 0x8000); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Free(): could not unallocate %p [Err: %s]", &b[0], err))
+	}
+
+	// Set it to nil.
+	b = nil
+	_ = b
+}
+
+// Protect modifies the PROT_ flags for a specified byte slice.
+func Protect(b []byte, prot uint32) *uint32 {
+	var oldProtect uint32
+	if err := winapi.VirtualProtect(_getPtr(b), uintptr(len(b)), uint32(prot), &oldProtect); err != nil {
+		panic(fmt.Sprintf("memguard.memcall.Protect(): could not set %d on %p [Err: %s]", prot, &b[0], err))
+	}
+	// Return the old value to the caller.
+	return oldProtect
+}
+
+func _getPtr(b []byte) {
 	var _p0 unsafe.Pointer
 	if len(b) > 0 {
 		_p0 = unsafe.Pointer(&b[0])
 	} else {
 		_p0 = unsafe.Pointer(&_zero)
 	}
-	if err := windows.VirtualUnlock(uintptr(_p0), uintptr(len(b))); err != nil {
-		panic(fmt.Sprintf("memguard.memcall.Unlock(): could not free lock on %p", &b[0]))
-	}
+	return uintptr(_p0)
+}
+
+func _getBytes(ptr uintptr, len int, cap int) []byte {
+	var sl = struct {
+		addr uintptr
+		len  int
+		cap  int
+	}{ptr, n, n}
+	return *(*[]byte)(unsafe.Pointer(&sl))
 }
