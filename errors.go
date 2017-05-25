2edf9cf3b41aec39dbfdaa22e366b5a9c5ba927d
Awn <awn@cryptolosophy.io>
2017-05-25 10:14:06 +0100

    Make error messages more explicit

--- errors.go
+++ errors.go
@@ -3,11 +3,11 @@ package memguard
 import "errors"
 
 // ErrInvalidLength is returned when a LockedBuffer of smaller than one byte is requested.
-var ErrInvalidLength = errors.New("memguard.Err: length of buffer must be greater than zero")
+var ErrInvalidLength = errors.New("memguard.ErrInvalidLength: length of buffer must be greater than zero")
 
 // ErrReadOnly is returned when a function that needs to modify a LockedBuffer
 // is given a LockedBuffer that is marked as being read-only.
-var ErrReadOnly = errors.New("memguard.Err: buffer is marked read-only")
+var ErrReadOnly = errors.New("memguard.ErrReadOnly: buffer is marked read-only")
 
 // ErrDestroyed is returned when a function is called on a destroyed LockedBuffer.
-var ErrDestroyed = errors.New("memguard.Err: buffer is destroyed")
+var ErrDestroyed = errors.New("memguard.ErrDestroyed: buffer is destroyed")
