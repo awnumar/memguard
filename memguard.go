263caace8331336a3623a43ebab6bcf4254d962b
Awn <awn@cryptolosophy.io>
2018-04-28 15:16:27 +0100

    feature: re-add the wipebytes function
    Re-add the WipeBytes function in light of the issue raised in #58. Also added a warning about its potential limitations to its documentation.
    
    The implementation has also been improved.

--- memguard.go
+++ memguard.go
@@ -804,6 +804,15 @@ func Trim(b *LockedBuffer, offset, size int) (*LockedBuffer, error) {
 }
 
 /*
+WipeBytes zeroes out a given byte slice. It is recommeded that you call WipeBytes on slices after utilizing the Copy or CopyAt methods.
+
+Due to the nature of memory allocated by the Go runtime, WipeBytes cannot guarantee that the data does not exist elsewhere in memory. Therefore, your program should aim to (when possible) store sensitive data only in LockedBuffers.
+*/
+func WipeBytes(b []byte) {
+	wipeBytes(b)
+}
+
+/*
 DestroyAll calls Destroy on all LockedBuffers that have not already been destroyed.
 
 CatchInterrupt and SafeExit both call DestroyAll before exiting.
