641df2107c8c1b70d8739ea3a3309e5edb6b83fc
Awn Umar <awn@spacetime.dev>
2019-03-22 02:50:10 +0000

    Add test for CatchInterrupt that currently catches 6 data races.

---
+++ memguard_test.go
@@ -0,0 +1,34 @@
+package memguard
+
+import (
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestCatchInterrupt(t *testing.T) {
+	// Start a listener object
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		SafePanic(err)
+	}
+	defer listener.Close()
+
+	// Spawn a handler.
+	CatchInterrupt(func() {
+		listener.Close()
+	})
+
+	// Grab a handle on the running process
+	process, err := os.FindProcess(os.Getpid())
+	if err != nil {
+		t.Error(nil)
+	}
+
+	// Send it an interrupt signal
+	if err := process.Signal(os.Interrupt); err != nil {
+		t.Error(err)
+	}
+	time.Sleep(8 * time.Second)
+}
