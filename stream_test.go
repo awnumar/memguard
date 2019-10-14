3dea8c1d676e750c68698dc51d5a9128882901b0
Awn Umar <awn@spacetime.dev>
2019-10-14 14:11:57 +0100

    tests: cleanup test code

--- stream_test.go
+++ stream_test.go
@@ -9,6 +9,33 @@ import (
 	"github.com/awnumar/memguard/core"
 )
 
+func write(t *testing.T, s *Stream, b []byte) {
+	n, err := s.Write(b)
+	if err != nil {
+		t.Error("write should always succeed", err)
+	}
+	if n != len(b) {
+		t.Error("not all data was written")
+	}
+	if !bytes.Equal(b, make([]byte, len(b))) {
+		t.Error("buffer not wiped")
+	}
+}
+
+func read(t *testing.T, s *Stream, ref []byte, expectedErr error) {
+	b := make([]byte, len(ref))
+	n, err := s.Read(b)
+	if err != expectedErr {
+		t.Error("Expected", expectedErr, "got;", err)
+	}
+	if n != len(b) {
+		t.Error("not enough data read")
+	}
+	if !bytes.Equal(ref, b) {
+		t.Error("data mismatch")
+	}
+}
+
 func TestStreamReadWrite(t *testing.T) {
 	// Create new stream object.
 	s := NewStream()
@@ -20,34 +47,13 @@ func TestStreamReadWrite(t *testing.T) {
 	copy(ref, b)
 
 	// Write the data to the stream.
-	n, err := s.Write(b)
-	if err != nil {
-		t.Error("write should always succeed", err)
-	}
-	if n != len(b) {
-		t.Error("not all data was written")
-	}
-	if !bytes.Equal(b, make([]byte, len(b))) {
-		t.Error("buffer not wiped")
-	}
+	write(t, s, b)
 
 	// Read the data back.
-	n, err = s.Read(b)
-	if err != nil {
-		t.Error(err)
-	}
-	if !bytes.Equal(ref, b) {
-		t.Error("data mismatch")
-	}
+	read(t, s, ref, nil)
 
 	// Check for end of data error
-	n, err = s.Read(b)
-	if err != io.EOF {
-		t.Error("expected EOF")
-	}
-	if n != 0 {
-		t.Error("expected no data")
-	}
+	read(t, s, nil, io.EOF)
 
 	// Write more than the pagesize to the stream
 	b = make([]byte, os.Getpagesize()*4+16)
@@ -55,115 +61,38 @@ func TestStreamReadWrite(t *testing.T) {
 	copy(b[os.Getpagesize()*4:], []byte("yellow submarine"))
 	ref = make([]byte, len(b))
 	copy(ref, b)
-	n, err = s.Write(b)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != len(b) {
-		t.Error("not all the data written")
-	}
-	if !bytes.Equal(b, make([]byte, len(b))) {
-		t.Error("buffer not wiped")
-	}
+	write(t, s, b)
 
 	// Read back four pages
 	for i := 0; i < 4; i++ {
-		c := make([]byte, os.Getpagesize())
-		n, err = s.Read(c)
-		if err != nil {
-			t.Error(err)
-		}
-		if n != os.Getpagesize() {
-			t.Error("incorrect amount of data read")
-		}
-		if !bytes.Equal(c, ref[i*os.Getpagesize():(i+1)*os.Getpagesize()]) {
-			t.Error("data mismatch")
-		}
+		read(t, s, ref[i*os.Getpagesize():(i+1)*os.Getpagesize()], nil)
 	}
 
 	// Read back the remaining data
-	data := make([]byte, 16)
-	n, err = s.Read(data)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != 16 {
-		t.Error("not enough data read")
-	}
-	if !bytes.Equal(data, []byte("yellow submarine")) {
-		t.Error("data mismatch")
-	}
+	read(t, s, []byte("yellow submarine"), nil)
 
 	// Should be no data left
-	n, err = s.Read(data)
-	if err != io.EOF {
-		t.Error("expected end of file error")
-	}
-	if n != 0 {
-		t.Error("expected no data left")
-	}
+	read(t, s, nil, io.EOF)
 
 	// Test reading less data than is in the chunk
+	data := make([]byte, 16)
 	ScrambleBytes(data)
 	ref = make([]byte, len(data))
 	copy(ref, data)
-	n, err = s.Write(data)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != len(data) {
-		t.Error("not enough data written")
-	}
-	if !bytes.Equal(data, make([]byte, len(data))) {
-		t.Error("buffer not wiped")
-	}
-	b = make([]byte, 8)
-	n, err = s.Read(b)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != len(b) {
-		t.Error("not enough data read")
-	}
-	if !bytes.Equal(b, ref[:8]) {
-		t.Error("data mismatch")
-	}
-	n, err = s.Read(b)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != len(b) {
-		t.Error("not enough data read")
-	}
-	if !bytes.Equal(b, ref[8:]) {
-		t.Error("data mismatch")
-	}
-	n, err = s.Read(data)
-	if err != io.EOF {
-		t.Error("expected end of file error")
-	}
-	if n != 0 {
-		t.Error("expected no data left")
-	}
+	write(t, s, data)
+	write(t, s, data) // have two enclaves in the stream, 32 bytes total
+	read(t, s, ref[:8], io.ErrShortBuffer)
+	read(t, s, ref[8:], nil)
+	read(t, s, make([]byte, 16), nil)
+	read(t, s, nil, io.EOF)
 
 	// Test reading after purging the session
 	ScrambleBytes(data)
-	n, err = s.Write(data)
-	if err != nil {
-		t.Error(err)
-	}
-	if n != len(data) {
-		t.Error("not enough data written")
-	}
+	write(t, s, data)
 	Purge()
-	n, err = s.Read(data)
-	if err != core.ErrDecryptionFailed {
-		t.Error("expected decryption failed error")
-	}
-	if n != 0 {
-		t.Error("expected zero bytes read")
-	}
-	if !bytes.Equal(data, make([]byte, len(data))) {
-		t.Error("expected data buffer empty")
-	}
+	read(t, s, nil, core.ErrDecryptionFailed)
+}
+
+func TestStreamingSanity(t *testing.T) {
+
 }
