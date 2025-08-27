b5808cec7c28848211401e7c9866690a508e97f4
Awn Umar <awn@spacetime.dev>
2025-08-27 23:00:48 +0100

    Replace use of depreciated SliceHeader with new unsafe.Slice

--- core/auxiliary.go
+++ core/auxiliary.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"os"
-	"reflect"
 	"unsafe"
 )
 
@@ -18,6 +17,5 @@ func roundToPageSize(length int) int {
 
 // Convert a pointer and length to a byte slice that describes that memory.
 func getBytes(ptr *byte, len int) []byte {
-	var sl = reflect.SliceHeader{Data: uintptr(unsafe.Pointer(ptr)), Len: len, Cap: len}
-	return *(*[]byte)(unsafe.Pointer(&sl))
+	return unsafe.Slice(ptr, len)
 }
